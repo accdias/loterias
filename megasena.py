f938535957c38ea5916bd35edaabd2ec744eb18d
Antonio Dias <accdias@gmail.com>
2019-01-22 09:41:59 -0200

    Move db actions into bs4 block

--- megasena.py
+++ megasena.py
@@ -11,27 +11,27 @@ megasena_html_file = 'D_MEGA.HTM'
 
 with requests.get(megasena_zip_file) as request:
     if request.ok:
-        db = sqlite3.connect('megasena.db')
-        db.execute('''
-            DROP TABLE IF EXISTS polls;
-            ''')
-        db.execute('''
-            CREATE TABLE polls (
-                id INTEGER NOT NULL PRIMARY KEY,
-                date DATE NOT NULL,
-                n1 INTEGER NOT NULL,
-                n2 INTEGER NOT NULL,
-                n3 INTEGER NOT NULL,
-                n4 INTEGER NOT NULL,
-                n5 INTEGER NOT NULL,
-                n6 INTEGER NOT NULL
-            );''')
         with ZipFile(BytesIO(request.content)).open(megasena_html_file) as html:
+            db = sqlite3.connect('megasena.db')
+            db.execute('''
+                DROP TABLE IF EXISTS polls;
+                ''')
+            db.execute('''
+                CREATE TABLE polls (
+                    id INTEGER NOT NULL PRIMARY KEY,
+                    date DATE NOT NULL,
+                    n1 INTEGER NOT NULL,
+                    n2 INTEGER NOT NULL,
+                    n3 INTEGER NOT NULL,
+                    n4 INTEGER NOT NULL,
+                    n5 INTEGER NOT NULL,
+                    n6 INTEGER NOT NULL
+                );''')
             soup = BeautifulSoup(html, 'html.parser')
             for tr in soup('tr'):
                 td = [tag.get_text() for tag in tr('td', limit=8, rowspan=True)]
                 if td and td[0].isdigit():
                     td[1] = datetime.strptime(td[1], '%d/%m/%Y')
                     db.execute('INSERT INTO polls VALUES (?, ?, ?, ?, ?, ?, ?, ?);', td)
-        db.commit()
-        db.close()
+            db.commit()
+            db.close()
